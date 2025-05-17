d980300a4869ebfd0350d4fa3c46b249ab5fa63f
Lidwina <lidwinaeleonora@gmail.com>
2025-05-17 09:00:46 +0700

    fix designer model index

--- app/Http/Controllers/Api/DesignerController.php
+++ app/Http/Controllers/Api/DesignerController.php
@@ -12,17 +12,20 @@ class DesignerController extends Controller
      */
     public function index()
     {
-        $designers = Designer::all()->map(function ($designer) {
-            return [
-                'id' => $designer->id,
-                'name' => $designer->name,
-                'country' => $designer->country,
-                'origin_city' => $designer->origin_city,
-                'specialty' => $designer->specialty,
-                'photo' => base64_encode($designer->photo),
-                'description' => $designer->description
-            ];
-        });
+        $designers = Designer::with(['user' => function($query) {
+                $query->select('id', 'name', 'country', 'photo');
+            }])
+            ->select('user_id', 'specialty')
+            ->get()
+            ->map(function ($designer) {
+                return [
+                    'id' => $designer->user->id,
+                    'name' => $designer->user->name,
+                    'country' => $designer->user->country,
+                    'specialty' => $designer->specialty,
+                    'photo' => $designer->user->photo ? base64_encode($designer->user->photo) : null
+                ];
+            });
 
         return response()->json($designers);
     }
