0d0a9eadeb963a61a798f650f103f6036a0e99a6
lidwinae <lidwinaeleonora@gmail.com>
2025-05-17 18:16:12 +0700

    penyesuaian migrasi role admin

--- database/migrations/2025_05_05_124058_create_mails_admin_table.php
+++ database/migrations/2025_05_05_124058_create_mails_admin_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->enum('judul', ['Feedback', 'Request menjadi designer', 'Request menjadi kontraktor'])->default('Feedback');
-            $table->enum('role', ['user', 'designer', 'kontraktor'])->default('user');
+            $table->enum('role', ['client', 'designer', 'contractor'])->default('client');
             $table->text('message');
             $table->mediumText('portfolio')->charset('binary')->nullable();
             $table->timestamps();
